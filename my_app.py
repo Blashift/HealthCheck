632b7462e0309c167fff98700413e259737e005d
Vadim <101970526+Blashift@users.noreply.github.com>
2022-04-17 15:57:36 +0500

    Update my_app

--- my_app.py
+++ my_app.py
@@ -1,58 +1,55 @@
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QWidget
-
-txt_title ='Здоровье'
-win_x, win_y = 200, 100
-win_width, win_height = 1000, 600
-txt_hello = 'Добро пожаловать в программу по определению состояния здоровья!'
-txt_instruction = 'Данное приложение позволит вам с помощью теста Руфье '
-txt_next = 'НАЧАТЬ'
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+ 
+from instr import *
+from second_win import *
+     
 class MainWin(QWidget):
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
-    def initUI(self):
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLable(txt_instruction)
-        self.button = QPushButton(txt_next)
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.hello_text)
-        self.layout.addWidget(self.instruction)
-        self.Layout.addWidget(self.button)
-    def connects (self):
-        self.bnt_next.clicked.connect(self.next_click)
-    def next_click(self):
-        self.window.hide()
-        self.tw =TestWin()
-    app = QCoreApplication([])
-    mw = MainWin(TestWin)
-    app.exec_()
-
-class MainWin(QWidget):
-    def set_appear(self):
-        pass
-    def initUI(self):
-        pass
-    def connects (self):
-        pass
-    def next_click(self):
-        self.window.hide()
-        self.tw =TestWin()
-
-class MainWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.set_appear(self)
-        self.initUI(self)
-        self.connects(self)
-        self.show()
-
-class TestWin(QWidget):
-    def __init__(self):
-        super().__init__(self)
-        self.set_appear()
-        self.initUI()
-        self.connects()
-        self.show()
+   def __init__(self):
+       ''' окно, в котором располагается приветствие '''
+       super().__init__()
+ 
+       # создаём и настраиваем графические элементы:
+       self.initUI()
+ 
+       #устанавливает связи между элементами
+       self.connects()
+ 
+       #устанавливает, как будет выглядеть окно (надпись, размер, место)
+       self.set_appear()
+ 
+       # старт:
+       self.show()
+ 
+   def initUI(self):
+       ''' создаёт графические элементы '''
+       self.btn_next = QPushButton(txt_next, self)
+       self.hello_text = QLabel(txt_hello)
+       self.instruction = QLabel(txt_instruction)
+ 
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+       self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+       self.setLayout(self.layout_line)
+  
+   def next_click(self):
+       self.tw = TestWin()
+       self.hide()
+ 
+   def connects(self):
+       self.btn_next.clicked.connect(self.next_click)
+ 
+   ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+   def set_appear(self):
+       self.setWindowTitle(txt_title)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
+ 
+app = QApplication([])
+mw = MainWin()
+app.exec_()
